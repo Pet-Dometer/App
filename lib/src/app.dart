98eb2b8946119dc5b622d7c60f7b4366ca909288
C Arakaki <ca234@hawaii.edu>
2023-10-03 12:30:03 -1000

    update

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:app/src/pages/creation.dart';
 import 'package:app/src/pages/home_view.dart';
+import 'package:app/src/pages/pedometer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -77,7 +78,7 @@ class MyApp extends StatelessWidget {
                     return const SampleItemDetailsView();
                   case SampleItemListView.routeName:
                   default:
-                    return CreationView();
+                    return PedometerView();
                 }
               },
             );
--- lib/src/pages/pedometer.dart
+++ lib/src/pages/pedometer.dart
@@ -10,5 +10,37 @@ class PedometerView extends StatefulWidget {
 }
 
 class _PedometerState extends State<PedometerView> {
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: 400,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.fill
+                ),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    height: 60.0,
+                    child: Text(
+                      'Mugi [13]',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                  Image.asset('assets/images/dog2.png', width: 300)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
