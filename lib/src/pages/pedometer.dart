5deefac7fc1ae2642e9c1b52457315e54c038b20
C Arakaki <ca234@hawaii.edu>
2023-10-03 10:51:00 -1000

    update

---
+++ lib/src/pages/pedometer.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PedometerView extends StatefulWidget {
+  const PedometerView({Key? key}) : super(key: key);
+
+  static const routeName = '/pedometer';
+
+  @override
+  _PedometerState createState() => _PedometerState();
+}
+
+class _PedometerState extends State<PedometerView> {
+
+}
