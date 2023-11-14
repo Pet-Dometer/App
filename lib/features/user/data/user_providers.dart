047adb1ae8785b56d211a7291a90a7d4065d76af
C Arakaki <ca234@hawaii.edu>
2023-11-13 20:46:34 -1000

    update

--- lib/features/user/data/user_providers.dart
+++ lib/features/user/data/user_providers.dart
@@ -1,11 +1,32 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide User;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../../../repositories-firestore/firestore_providers.dart';
 import '../domain/user.dart';
+import 'user_database.dart';
 
-final userDBProvider = Provider<UserDB>((ref) {
-  return UserDB(ref);
-});
+part 'user_providers.g.dart';
 
-final currentUserIDProvider = StateProvider<String>((ref) {
-  return 'user-001';
-});
+@riverpod
+UserDatabase userDatabase(UserDatabaseRef ref) {
+  return UserDatabase(ref);
+}
+
+@riverpod
+Stream<List<User>> users(UsersRef ref) {
+  final database = ref.watch(userDatabaseProvider);
+  return database.watchUsers();
+}
+
+@riverpod
+String currentUserID(CurrentUserIDRef ref) {
+  final FirebaseAuth instance = ref.watch(firebaseAuthProvider);
+  return instance.currentUser!.email!;
+}
+
+@riverpod
+Future<User> currentUser(CurrentUserRef ref) async {
+  final String currentUserId = ref.watch(currentUserIDProvider);
+  final database = ref.watch(userDatabaseProvider);
+  return await database.fetchUser(currentUserId);
+}
